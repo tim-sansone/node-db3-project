105ea480b7d987d5fd32ce623b7e7067da5e2e96
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-20 16:34:52 -0500

    get request functional

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,3 +1,6 @@
+const db = require('../../data/db-config');
+
+
 function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
@@ -15,6 +18,11 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+  return db('schemes')
+          .select('schemes.scheme_id as scheme_id', 'scheme_name')
+          .count('steps.step_id as number_of_steps')
+          .leftJoin('steps', 'schemes.scheme_id', 'steps.scheme_id')
+          .groupBy('schemes.scheme_id')
 }
 
 function findById(scheme_id) { // EXERCISE B
